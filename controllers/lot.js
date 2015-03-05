4730ce2677473471b98d403799f8dbf99e2f5e00
Ben Pritchard and Arsi <ben.s.pritchard@gmail.com>
2015-03-05 14:55:23 -0800

    Move the delete lot button to the lots page.

--- controllers/lot.js
+++ controllers/lot.js
@@ -1,10 +1,5 @@
 Auction.LotController = Ember.ObjectController.extend({
   actions: {
-    delete: function() {
-      if (confirm('Are you sure?')) {
-        this.get('model').destroyRecord();
-      }
-      this.transitionToRoute('lots');
-    }
+    
   }
 });
--- controllers/lots.js
+++ controllers/lots.js
@@ -1,7 +1,9 @@
 Auction.LotsController = Ember.ArrayController.extend ({
   actions: {
     deleteLot: function(lot) {
-      lot.destroyRecord();
+      if (confirm('Are you sure you want to delete this beautiful lot?')) {
+        lot.destroyRecord();
+      }
     }
   }
 })
