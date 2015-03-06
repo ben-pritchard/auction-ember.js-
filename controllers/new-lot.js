97c675d99685a13ace270c152d9de4d289642081
Ben Pritchard and Arsi <ben.s.pritchard@gmail.com>
2015-03-05 16:00:28 -0800

    Make a functional carousel that displays the image link property.

--- controllers/new-lot.js
+++ controllers/new-lot.js
@@ -3,7 +3,8 @@ Auction.NewLotController = Ember.Controller.extend({
     save: function() {
       var newLot = this.store.createRecord('lot', {
         name: this.get('lotName'),
-        description: this.get('lotDescription')
+        description: this.get('lotDescription'),
+        image: this.get("imageLink")
       });
       newLot.save();
       this.transitionToRoute('lots');
--- models/lot.js
+++ models/lot.js
@@ -1,5 +1,6 @@
 Auction.Lot = DS.Model.extend({
   name: DS.attr(),
   description: DS.attr(),
+  image: DS.attr(),
   swords: DS.hasMany('sword', {async: true})
 });
